3c99d96f733e38b944a70ecdbc0904f61b124372
yazan ali <yazanabuali2000@gmail.com>
2021-05-09 19:10:55 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { setContext } from "@apollo/client/link/context";
 import Routes from './routes/routes';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000/'
+  uri: 'https://ancient-inlet-09851.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
